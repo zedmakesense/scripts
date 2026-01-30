b2f27a5f7432dc82fc008821c83f5bca30ad969c
zedonarch <zedonix@proton.me>
2026-01-30 17:05:37 +0530

    top.sh - remove the processes running because of top.sh

--- bin/top.sh
+++ bin/top.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
+
 watch -n 1 '
 echo "=== TOP CPU (aggregate) ==="
-ps -eo comm,%cpu --no-headers | awk "{cpu[\$1]+=\$2} END{for (c in cpu) printf \"%6.2f%% %s\n\", cpu[c], c}" | sort -nr | head -n 12
+ps -eo pid,comm,%cpu --no-headers |
+awk '\''$2 !~ /^(ps|watch|awk|bash)$/ { cpu[$2] += $3 }
+     END { for (c in cpu) printf "%7.2f%% %s\n", cpu[c], c }'\'' |
+sort -nr | head -n 12
+
 echo
 echo "=== TOP MEM (aggregate) ==="
-ps -eo comm,%mem --no-headers | awk "{mem[\$1]+=\$2} END{for (c in mem) printf \"%6.2f%% %s\n\", mem[c], c}" | sort -nr | head -n 12
+ps -eo pid,comm,%mem --no-headers |
+awk '\''$2 !~ /^(ps|watch|awk|bash)$/ { mem[$2] += $3 }
+     END { for (c in mem) printf "%7.2f%% %s\n", mem[c], c }'\'' |
+sort -nr | head -n 12
 '
