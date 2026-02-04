f8f2cd8d4c0a3c5c23bb38f582eeee9346c84478
zedonarch <zedonix@proton.me>
2026-02-04 11:06:42 +0530

    feat: muting/unmuting with a keybind

---
+++ bin/mute.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+SOURCE="@DEFAULT_AUDIO_SOURCE@"
+
+ICON_MUTED="/usr/share/icons/Papirus/48x48/status/microphone-sensitivity-muted.svg"
+ICON_ON="/usr/share/icons/Papirus/48x48/status/microphone-sensitivity-high.svg"
+
+if wpctl get-volume "$SOURCE" | grep -q '\[MUTED\]'; then
+  wpctl set-mute "$SOURCE" 0
+  notify-send -i "$ICON_ON" "Microphone" "Unmuted"
+else
+  wpctl set-mute "$SOURCE" 1
+  notify-send -i "$ICON_MUTED" "Microphone" "Muted"
+fi
