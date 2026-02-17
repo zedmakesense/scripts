21d17f19ad722705542e56d80ea4fa9b2beb2f1f
zedonarch <zedonix@proton.me>
2026-02-17 10:10:51 +0530

    fixed: top.sh ignoring all bash scripts

--- bin/top.sh
+++ bin/top.sh
@@ -3,14 +3,14 @@
 watch -n 1 '
 echo "=== TOP CPU (aggregate) ==="
 ps -eo pid,comm,%cpu --no-headers |
-awk '\''$2 !~ /^(ps|watch|awk|bash)$/ { cpu[$2] += $3 }
+awk '\''$2 !~ /^(ps|watch|awk)$/ { cpu[$2] += $3 }
      END { for (c in cpu) printf "%7.2f%% %s\n", cpu[c], c }'\'' |
 sort -nr | head -n 12
 
 echo
 echo "=== TOP MEM (aggregate) ==="
 ps -eo pid,comm,%mem --no-headers |
-awk '\''$2 !~ /^(ps|watch|awk|bash)$/ { mem[$2] += $3 }
+awk '\''$2 !~ /^(ps|watch|awk)$/ { mem[$2] += $3 }
      END { for (c in mem) printf "%7.2f%% %s\n", mem[c], c }'\'' |
 sort -nr | head -n 12
 '
